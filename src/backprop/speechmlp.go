915345260ca80dfa3b7ef1bcbde6ed20effb016c
thomasteplick <thomas.teplick@hotmail.com>
2024-07-29 15:58:28 -0700

    Normalize the PSD in calculatePSD, otherwise the activation function saturates and there is no convergence.

--- src/backprop/speechmlp.go
+++ src/backprop/speechmlp.go
@@ -1278,6 +1278,10 @@ func (mlp *MLP) calculatePSD(audio []float64, PSD []float64, plottype, fftWindow
 				psdMin = PSD[i]
 			}
 		}
+		// Normalize to 1, otherwise the activation function saturates
+		for i := range PSD {
+			PSD[i] /= psdMax
+		}
 		// 10log10 in dB
 	} else if plottype == "log" {
 		for i := range PSD {
